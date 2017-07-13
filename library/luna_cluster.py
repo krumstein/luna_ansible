b68b9652e47ae7713f4650192b693085c5840ef2
Dmitry Chirikov <dmitry@chirikov.ru>
2017-07-13 22:53:41 +0200

    makedhcp and makedns actions, PEP8 compliant

--- library/luna_cluster.py
+++ library/luna_cluster.py
@@ -1,103 +1,192 @@
 #!/usr/bin/python
 
 from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils.pycompat24 import get_exception
-import json
 import luna
-import sys
 import traceback
 
+
 def luna_cluster_present(data):
-    #for k, v in data.items():
-    #     exec('%s = v' % k)
     data.pop('state')
-    try:
-        cluster = luna.Cluster()
-    except RuntimeError:
-        init = { key: data[key] for key in data if key in ['nodeprefix' , 'nodedigits' , 'path' , 'user'] }
-        init['create'] = True
-        cluster = luna.Cluster(**init)
-        return False, True, str(cluster)
+    makedhcp = data.pop('makedhcp')
+    makedns = data.pop('makedns')
+    dhcp_net = data.pop('dhcp_net')
+    dhcp_range_start = data.pop('dhcp_range_start')
+    dhcp_range_end = data.pop('dhcp_range_end')
+    native_dhcp_ha = data.pop('native_dhcp_ha')
+
+    changed = False
 
     try:
-            cluster = luna.Cluster()
-            changed = False
-            out = ""
-            for key in data:
-                if data[key]!= None and cluster.get(key) != data[key]:
-                    out += "{}={},".format(key,data[key])
-                    changed = True
-                    cluster.set(key, data[key])   
-            return False, changed, str(cluster) + out
+        cluster = luna.Cluster()
+
+    except RuntimeError:
+        init = {}
+        for key in data:
+            if key in ['nodeprefix', 'nodedigits', 'path', 'user']:
+                init[key] = data[key]
+        init['create'] = True
+        cluster = luna.Cluster(**init)
+        changed = True
+
+    try:
+        cluster = luna.Cluster()
+        out = ''
+
+        for key in data:
+            if data[key] is not None and cluster.get(key) != data[key]:
+                out += '{}={},'.format(key, data[key])
+                changed = True
+                cluster.set(key, data[key])
+
+        if makedns:
+            if not cluster.makedns():
+                return True, False, 'Unable to build DNS config'
+            changed = True
+
+        if makedhcp:
+            no_ha = not native_dhcp_ha
+
+            if dhcp_net:
+                if not (dhcp_range_start and dhcp_range_end):
+                    return True, False, ('Unable to build DHCP config. ' +
+                                         'Network range should be specified')
+
+            else:
+                if dhcp_range_start or dhcp_range_end:
+                    return True, False, ('Unable to build DHCP config. ' +
+                                         'Network should be specified.')
+
+            need_to_rebuild_dhcp = False
+
+            old_dhcp_net = cluster.get('dhcp_net')
+            old_dhcp_range_start = cluster.get('dhcp_range_start')
+            old_dhcp_range_end = cluster.get('dhcp_range_end')
+
+            if (old_dhcp_net != dhcp_net
+                    or old_dhcp_range_start != dhcp_range_start
+                    or old_dhcp_range_end != dhcp_range_end):
+
+                need_to_rebuild_dhcp = True
+
+            if need_to_rebuild_dhcp:
+
+                if not cluster.makedhcp(
+                        dhcp_net, dhcp_range_start, dhcp_range_end, no_ha):
+
+                    return True, False, 'Unable to build DHCP config.'
+
+                changed = True
+
+        return False, changed, str(cluster) + out
     except Exception as e:
         return True, False, str(e) + traceback.format_exc()
 
-#    try:
-#        if name not in clusters:
-#            data['create'] = True
-#            cluster = luna.BMCSetup(**data)
-#            return False, True, str(cluster)
-#        else:
-#            cluster = luna.BMCSetup(name = name)
-#            changed = False
-#            for key in data:
-#                if data[key]!= None and cluster.get(key) != data[key]:
-#                    changed = True
-#                    cluster.set(key, data[key])   
-#            return False, changed, str(cluster)
-#    except Exception as e:
-#        return True, False, str(e) + traceback.format_exc()
-#
+
 def luna_cluster_absent(data):
     for k, v in data.items():
-         exec('%s = v' % k)
+        exec('%s = v' % k)
     try:
         cluster = luna.Cluster()
         cluster.delete()
-        return False, True, ""
+        return False, True, ''
     except Exception as e:
-        return True, False, str(e)+ traceback.format_exc()
-
+        return True, False, str(e) + traceback.format_exc()
 
 
 def main():
     module = AnsibleModule(
-        argument_spec = dict(
-            nodeprefix               = dict(type="str",  default="node",   required=False),
-            nodedigits               = dict(type="str",  default="3",      required=False),
-            user                     = dict(type="str",  default="luna",   required=False),
-            path                     = dict(type="str",  default=None,     required=False),
-            frontend_address         = dict(type="str",  default=None,     required=False),
-            frontend_port            = dict(type="int",  default=None,     required=False),
-            server_port              = dict(type="int",  default=None,     required=False),
-            tracker_interval         = dict(type="int",  default=None,     required=False),
-            tracker_min_interval     = dict(type="int",  default=None,     required=False),
-            tracker_maxpeers         = dict(type="int",  default=None,     required=False),
-            torrent_listen_port_min  = dict(type="int",  default=None,     required=False),
-            torrent_listen_port_max  = dict(type="int",  default=None,     required=False),
-            torrent_pidfile          = dict(type="str",  default=None,     required=False),
-            lweb_num_proc            = dict(type="int",  default=None,     required=False),
-            lweb_pidfile             = dict(type="str",  default=None,     required=False),
-            cluster_ips              = dict(type="str",  default=None,     required=False),
-            named_include_file       = dict(type="str",  default=None,     required=False),
-            named_zone_dir           = dict(type="str",  default=None,     required=False),
-            
-            makedhcp                 = dict(type="bool", default=False,    required=False),
-            no_ha                    = dict(type="bool", default=True,     required=False),
-            network                  = dict(type="str",  default=None,     required=False),
-            start_ip                 = dict(type="str",  default=None,     required=False),
-            end_ip                   = dict(type="str",  default=None,     required=False),
+        argument_spec={
 
-            makedns                  = dict(type="bool", default=False,    required=False),
+            'nodeprefix': {
+                'type': 'str',  'default': 'node',   'required': False},
 
-            state                    = dict(type="str",  default="present",
-                                                         choices=["present", "absent"] )
-            )
+            'nodedigits': {
+                'type': 'str',  'default': '3',      'required': False},
+
+            'user': {
+                'type': 'str',  'default': 'luna',   'required': False},
+
+            'path': {
+                'type': 'str',  'default': None,     'required': False},
+
+            'frontend_address': {
+                'type': 'str',  'default': None,     'required': False},
+
+            'frontend_port': {
+                'type': 'int',  'default': None,     'required': False},
+
+            'server_port': {
+                'type': 'int',  'default': None,     'required': False},
+
+            'tracker_interval': {
+                'type': 'int',  'default': None,     'required': False},
+
+            'tracker_min_interval': {
+                'type': 'int',  'default': None,     'required': False},
+
+            'tracker_maxpeers': {
+                'type': 'int',  'default': None,     'required': False},
+
+            'torrent_listen_port_min': {
+                'type': 'int',  'default': None,     'required': False},
+
+            'torrent_listen_port_max': {
+                'type': 'int',  'default': None,     'required': False},
+
+            'torrent_pidfile': {
+                'type': 'str',  'default': None,     'required': False},
+
+            'lweb_num_proc': {
+                'type': 'int',  'default': None,     'required': False},
+
+            'lweb_pidfile': {
+                'type': 'str',  'default': None,     'required': False},
+
+            'cluster_ips': {
+                'type': 'str',  'default': None,     'required': False},
+
+            'named_include_file': {
+                'type': 'str',  'default': None,     'required': False},
+
+            'named_zone_dir': {
+                'type': 'str',  'default': None,     'required': False},
+
+            'comment': {
+                'type': 'str',  'default': None,     'required': False},
+
+            # dhcpmake related parameters
+
+            'dhcp_net': {
+                'type': 'str',  'default': None,     'required': False},
+
+            'dhcp_range_start': {
+                'type': 'str',  'default': None,     'required': False},
+
+            'dhcp_range_end': {
+                'type': 'str',  'default': None,     'required': False},
+
+            'native_dhcp_ha': {
+                'type': 'bool', 'default': False,    'required': False},
+
+            # actions
+
+            'makedns': {
+                'type': 'bool', 'default': False,    'required': False},
+
+            'makedhcp': {
+                'type': 'bool', 'default': False,    'required': False},
+
+            'state': {
+                'type': 'str',
+                'default': 'present',
+                'choices': ['present', 'absent']
+            }
+        }
     )
-    
+
     choice_map = {
-        "present": luna_cluster_present,
-        "absent": luna_cluster_absent,
+        'present': luna_cluster_present,
+        'absent': luna_cluster_absent,
     }
 
     is_error, has_changed, result = choice_map.get(
@@ -106,8 +195,8 @@ def main():
     if not is_error:
         module.exit_json(changed=has_changed, meta=result)
     else:
-        module.fail_json(msg="Error osimage changing", meta=result)
-    
+        module.fail_json(msg='Error cluster changing', meta=result)
 
-if __name__ == '__main__':  
+
+if __name__ == '__main__':
     main()
