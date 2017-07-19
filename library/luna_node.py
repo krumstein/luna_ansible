e110af95eb546598954b768009b91d667d79e9a0
Dmitry Chirikov <dmitry@chirikov.ru>
2017-07-19 17:17:13 +0200

    Node module

--- library/luna_node.py
+++ library/luna_node.py
@@ -1,77 +1,130 @@
 #!/usr/bin/python
 
 from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils.pycompat24 import get_exception
-import json
 import luna
-import sys
+
 
 def luna_node_present(data):
-        name = data['name']
-        group = data['group']
-        interfaces = data['interfaces']
-        mac = data['mac']
-        switch = data['switch']
-        port = data['port']
-        localboot = data['localboot'] 
-        service = data['service'] 
-        setupbmc = data['setupbmc'] 
-        comment = data['comment']
-        
-        nodes = luna.list('node')
-        msg = ""
-    #    try:
-        if name not in nodes:
-            node = luna.Node(name = name, create = True, group = group)
+    name = data.pop('name')
+    changed = False
+    ret = True
 
-            node.set('localboot', localboot)
-            node.set('service', service)
-            node.set('setupbmc', setupbmc)
-            if port:
-                node.set('port', port)
-            if switch:
-                node.set_switch( switch)
-            if mac:
-                node.set_mac(mac)
+    try:
+        node = luna.Node(name=name)
+    except RuntimeError:
+        if data['group'] is None:
+            err_msg = "Group needs to be specified to create the node"
+            return True, changed, err_msg
+        args = {
+            'name': name,
+            'create': True,
+            'group': data['group'],
+            'localboot': data['localboot'],
+            'setupbmc': data['setupbmc'],
+            'service': data['service'],
+            'comment': data['comment'],
+        }
+        node = luna.Node(**args)
+        changed = True
 
-            return False, True, str(node)
-#    except Exception as e:
-#        return True, False, str(e)
+    ansible_ifs = [e['name'] for e in data['interfaces']]
+    node_ifs = node.list_ifs().keys()
+    undefined_ifs = [e for e in ansible_ifs if e not in node_ifs]
+
+    if undefined_ifs:
+        err_msg = ("Node does not have {} interfaces configured."
+                   .format(" ".join(undefined_ifs)))
+        return True, changed, err_msg
+
+    keys = ['localboot', 'setupbmc', 'service', 'port', 'comment']
+    for key in keys:
+        if data[key] is None:
+            continue
+        if node.get(key) == data[key]:
+            continue
+        ret &= node.set(key, data[key])
+        changed = True
+
+    node_show = node.show()
+    keys = ['group', 'switch']
+    for key in keys:
+        if (data[key] is not None
+                and node_show[key] != '[' + data[key] + ']'):
+            ret &= getattr(node, "set_%s" % key)(data[key])
+            changed = True
+
+    if (data['mac'] is not None
+            and node.get_mac != data['mac']):
+        ret &= node.set_mac(data['mac'])
+        changed = True
+
+    for e in data['interfaces']:
+        ifname = e['name']
+        ansible_ips = e['ips']
+        conf_ips = []  # configured IPs
+
+        for i in [4, 6]:
+            ip = node.get_ip(ifname, version=i, quiet=True)
+
+            if ip is None:
+                continue
+
+            conf_ips.append(ip)
+
+        ips_to_change = [i for i in ansible_ips if i not in conf_ips]
+        for ip in ips_to_change:
+
+            ret &= node.set_ip(
+                interface_name=ifname,
+                ip=ip)
+
+            changed = True
+
+    return not ret, changed, str(node)
+
 
 def luna_node_absent(data):
-    locals().update(data)
-    nodes = luna.list('node')
+    name = data['name']
     try:
-        if name not in nodes:
-            return False, False, name
-        else:
-            node = luna.Node(name)
-            node.delete()
-            return False, True, name
-    except Exception as e:
-        return True, False, str(e)
+        node = luna.Node(name=name)
+    except RuntimeError:
+        return False, False, name
+
+    return not node.delete(), True, name
+
 
 def main():
     module = AnsibleModule(
-        argument_spec = dict(
-            name          = dict(type="str", required=True),
-            group         = dict(type="str", required=True),
-            comment       = dict(type="str", default="", required=False),
-            interfaces    = dict(type="list", default=[], required=False),
-            localboot     = dict(type="bool", default=False, required=False),
-            setupbmc      = dict(type="bool", default=True, required=False),
-            service       = dict(type="bool", default=False, required=False),
-            mac           = dict(type="str", default="",required=False),
-            switch        = dict(type="str",  default="",required=False),
-            port          = dict(type="int",  default=0,required=False),
-            state         = dict(type="str", default="present",
-                                             choices=['present', 'absent'] )
-            )
+        argument_spec={
+            'name': {
+                'type': 'str', 'default': None, 'required': True},
+            'group': {
+                'type': 'str', 'default': None, 'required': False},
+            'comment': {
+                'type': 'str', 'default': None, 'required': False},
+            'interfaces': {
+                'type': 'list', 'default': [], 'required': False},
+            'localboot': {
+                'type': 'bool', 'default': None, 'required': False},
+            'setupbmc': {
+                'type': 'bool', 'default': None, 'required': False},
+            'service': {
+                'type': 'bool', 'default': None, 'required': False},
+            'mac': {
+                'type': 'str', 'default': None, 'required': False},
+            'switch': {
+                'type': 'str', 'default': None, 'required': False},
+            'port': {
+                'type': 'int', 'default': None, 'required': False},
+            'state': {
+                'type': 'str', 'default': 'present',
+                'choices': ['present', 'absent']}
+        }
     )
-    
+
     choice_map = {
-        "present": luna_node_present,
-        "absent": luna_node_absent,
+        'present': luna_node_present,
+        'absent': luna_node_absent,
     }
 
     is_error, has_changed, result = choice_map.get(
@@ -80,8 +133,8 @@ def main():
     if not is_error:
         module.exit_json(changed=has_changed, meta=result)
     else:
-        module.fail_json(msg="Error node changing", meta=result)
-    
+        module.fail_json(msg='Error node changing', meta=result)
 
-if __name__ == '__main__':  
+
+if __name__ == '__main__':
     main()
